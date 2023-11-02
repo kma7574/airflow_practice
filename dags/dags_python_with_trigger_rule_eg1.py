4ce4b575b3784f9a05337f3c246bf83b9cad4a01
JaeYunJung <kma7574@naver.com>
2023-11-02 19:53:25 +0900

    주석 추가

---
+++ dags/dags_python_with_trigger_rule_eg1.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from airflow.exceptions import AirflowException
+
+import pendulum
+
+with DAG(
+    dag_id='dags_python_with_trigger_rule_eg1',
+    start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
+    schedule=None,
+    catchup=False
+) as dag:
+    
+    bash_upstream_1 = BashOperator(
+        task_id='bash_upstream_1',
+        bash_command='echo upstream1'
+    )
+
+    @task(task_id='python_upstream_1')
+    def python_upstream_1():
+        raise AirflowException('downstream_1 Exception!')  # 해당 task를 'failed'가 뜨게함
+
+
+    @task(task_id='python_upstream_2')
+    def python_upstream_2():
+        print('정상 처리')
+
+    @task(task_id='python_downstream_1', trigger_rule='all_done')  
+    # trigger rule 추가로 상위 3개 task가 실행된 후, 해당 task가 실행될지 안될지 결정됨
+    def python_downstream_1():
+        print('정상 처리')
+
+    [bash_upstream_1, python_upstream_1(), python_upstream_2()] >> python_downstream_1()
